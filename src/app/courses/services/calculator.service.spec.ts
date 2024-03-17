9db3edb37bc8c856ed33d34d2c769b66a96b9b94
Sahil Sharma <sahil.sharma1912@gmail.com>
2024-03-17 19:47:14 +0530

    FIRST :: commit in 1-start

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,46 @@
+import {CalculatorService} from "./calculator.service";
+import {LoggerService} from "./logger.service";
+import {TestBed} from "@angular/core/testing";
+
+describe('CalculatorService', () => {
+
+  let calculator: CalculatorService = undefined, loggerSpy: any;
+
+  beforeEach(() => {
+
+    console.log('Calling before each');
+
+    loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        CalculatorService,
+        {provide: LoggerService, useValue: loggerSpy}
+      ]
+    });
+
+    calculator = TestBed.inject(CalculatorService);
+  });
+
+  it('Should add two numbers', () => {
+
+    console.log('add test');
+
+    const result = calculator.add(15, 20);
+
+    expect(result).toBe(35);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+
+  })
+
+  it('Should subtract two numbers', () => {
+
+    console.log('subtract test');
+
+    const result = calculator.subtract(20, 15);
+
+    expect(result).toBe(5);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+
+  })
+})
